0c3263997b63314472176b7ae8d2880ce79bb371
inas <anishainas@gmail.com>
2020-05-10 02:39:10 +0700

    setup firebase

--- app/src/main/java/inas/anisha/srl_app/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/inas/anisha/srl_app/ui/onboarding/OnboardingActivity.kt
@@ -1,16 +1,20 @@
 package inas.anisha.srl_app.ui.onboarding
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import inas.anisha.srl_app.R
 import inas.anisha.srl_app.databinding.ActivityOnboardingBinding
 import inas.anisha.srl_app.ui.signup.SignUpActivity
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
+
 
 class OnboardingActivity : AppCompatActivity() {
 
@@ -58,6 +62,28 @@ class OnboardingActivity : AppCompatActivity() {
                 mViewModel.pageNum.value = position
             }
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
+        val dialogClickListener =
+            DialogInterface.OnClickListener { dialog, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+                        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
+                    }
+                    DialogInterface.BUTTON_NEGATIVE -> {
+                        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
+                    }
+                }
+            }
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setMessage("Izinkan aplikasi mengumpulkan data untuk keperluan penelitian? (Kerahasiaan data dijamin oleh peneliti)")
+            .setCancelable(false)
+            .setPositiveButton("Izinkan", dialogClickListener)
+            .setNegativeButton("Jangan Izinkan", dialogClickListener).show()
     }
 
     override fun attachBaseContext(newBase: Context?) {
